bf7dc80532c8cff86e933267d1748771917a7521
Ivan Bulyk <stackyogi@gmail.com>
2022-07-17 20:38:37 +0200

    removed URL)

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -6,9 +6,9 @@ export const Featured = () => {
     const [index, setIndex] = useState(0);
   const images = [
     "/img/pizza.png",
-    "/img/pizza1.png",
-    "/img/pizza2.png",
-    "/img/pizza3.png",
+    "/img/pizza4.png",
+    "/img/pizza5.png",
+    "/img/pizza6.png",
   ];
   const handleArrow = (direction) =>{
       if(direction==="l"){
