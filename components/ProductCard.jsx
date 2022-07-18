233d8d6a8b0c07bc471b9e9e4cc7af1987e7ef1e
Ivan Bulyk <stackyogi@gmail.com>
2022-07-18 15:46:26 +0200

    added product switching

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/ProductCard.module.css";
+import Link from "next/link";
 
 export const ProductCard = ({ product }) => {
   return (
     <div className={styles.container}>
-      <Image src={product.img} alt="" width="500" height="500" />
+      <Link href={`/product/${product._id}`} passHref>
+        <Image src={product.img} alt="" width="500" height="500" />
+      </Link>
       <h1 className={styles.title}>{product.title}</h1>
       <span className={styles.price}>${product.prices[0]}</span>
       <p className={styles.desc}>
